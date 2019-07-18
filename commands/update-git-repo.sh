dcb0e234e71b9211f7e73a2f8d087374c803eb3e
Sachin Tewari <xachin@gmail.com>
2019-07-19 00:43:15 +0800

    Fallback to origin remote if upstream is not defined

--- commands/update-git-repo.sh
+++ commands/update-git-repo.sh
@@ -15,8 +15,18 @@ fi
 # Checkout master
 git checkout master || exit 1
 
-# Pull upstream master
-git pull upstream master || exit 1
+# Check if upstream remote is defined
+if git remote get-url upstream > /dev/null 2>&1; then
+	# Pull upstream master
+	git pull upstream master || exit 1
+# Check if origin remote is defined
+elif git remote get-url origin > /dev/null 2>&1; then
+	# Pull origin master
+	git pull origin master || exit 1
+else
+	echo "both upstream and origin remotes undefined";
+	exit 1
+fi
 
 # Fetch all changes
 git fetch --all --prune || exit 1

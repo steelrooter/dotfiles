dde78b0ce2062c435976c207018b823ec06c824d
Sachin Tewari <xachin@gmail.com>
2019-07-19 00:23:09 +0800

    Check if inside a valid git repo before update attempt

--- commands/update-git-repo.sh
+++ commands/update-git-repo.sh
@@ -1,3 +1,11 @@
+# Check if inside a valid git repo
+if git rev-parse --git-dir > /dev/null 2>&1; then
+	: # Inside a git repo
+else
+	echo "not a git repo";
+	exit 1
+fi
+
 # Check if repo is dirty
 if [[ -n $(git status --porcelain) ]]; then
 	echo "repo is dirty";
